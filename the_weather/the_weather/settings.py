205ce8e7cdee12cb9ee986b1382d4bed360b1403
Mayank Marwaha <bunnymarwaha.bm@gmail.com>
2020-07-25 19:47:18 -0600

    changes to settings.py

--- the_weather/the_weather/settings.py
+++ the_weather/the_weather/settings.py
@@ -81,8 +81,13 @@ WSGI_APPLICATION = 'the_weather.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mayank',
+        'USER': 'mayank',
+        'PASSWORD': 'mayank',
+        'HOST': 'localhost',
+        'PORT': '',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
